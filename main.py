4ff72312ece9e6145ad965e461c832a9a7ac274a
Josh <zylo.codes@gmail.com>
2022-09-27 21:59:17 -0500

    movement

--- main.py
+++ main.py
@@ -23,12 +23,28 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
+class Ship:
+  def __init__(self, x, y, health = 100):
+    self.x = x
+    self.y = y
+    self.health = health
+    self.ship_image = None
+    self.laser_img = None
+    self.lasers = []
+    self.cool_down_counter = 0
+
+  def draw(self, window):
+    pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50), 0)
+
 def main():
   run = True
   FPS = 60
   level = 1
   lives = 5
+  player_vel = 5
   main_font = pygame.font.SysFont("comicsans", 50)
+
+  ship = Ship(300, 650)
 
   clock = pygame.time.Clock()
   
@@ -41,6 +57,8 @@ def main():
     WIN.blit(lives_label, (10, 10))
     WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
+    ship.draw(WIN)
+
     pygame.display.update()
 
   while run:
@@ -50,5 +68,15 @@ def main():
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         run = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a] and ship.x - player_vel > 0:
+      ship.x -= player_vel
+    if keys[pygame.K_d] and ship.x + player_vel + 50 < WIDTH:
+      ship.x += player_vel
+    if keys[pygame.K_w] and ship.y > 0:
+      ship.y -= player_vel
+    if keys[pygame.K_s] and ship.y + player_vel + 50 < HEIGHT:
+      ship.y += player_vel
 
 main()
