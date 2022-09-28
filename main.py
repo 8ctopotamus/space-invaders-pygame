a078608ccc9d7a3e3bbb51fefa30084797b8c178
Josh <zylo.codes@gmail.com>
2022-09-27 21:16:33 -0500

    background

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from turtle import width
 import pygame
 import os
 import time
 import random
 
-WIDTH, HEIGHT = 750, 750
+pygame.font.init()
+
+WIDTH, HEIGHT = 800, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Shooter")
 
@@ -20,22 +21,33 @@ GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
-BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 def main():
   run = True
   FPS = 60
+  level = 1
+  lives = 5
+  main_font = pygame.font.SysFont("comicsans", 50)
+
   clock = pygame.time.Clock()
   
   def redraw_window():
     WIN.blit(BG, (0, 0))
+    # draw text
+    lives_label = main_font.render("Lives: " + str(lives), 1, (255, 255, 255))
+    level_label = main_font.render("Level: " + str(level), 1, (255, 255, 255))
+
+    WIN.blit(lives_label, (10, 10))
+    WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
     pygame.display.update()
 
   while run:
     clock.tick(FPS)
+    redraw_window()
 
     for event in pygame.event.get():
-      redraw_window()
       if event.type == pygame.QUIT:
         run = False
 
